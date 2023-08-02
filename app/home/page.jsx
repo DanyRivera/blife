b865cf064472555342150838f0bd1ce944391455
Dany Rivera <dany@gmail.com>
2023-08-02 10:04:50 -0600

    Terminado barra y heading

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -12,12 +12,36 @@ const Home = () => {
                     height={500}
                     alt="bg-robot"
                 />
-                <div className="absolute top-0">
-                    <div className="flex m-10">
-                        <p>Hola</p>
-                        <p>Hola</p>
-                        <p>Hola</p>
-                        <p>Hola</p>
+                <div className="absolute top-0 w-full">
+                    <div className="flex justify-between items-center mx-20 py-14">
+                        <p className="text-3xl font-bold">noon</p>
+                        <div className="flex gap-16 font-bold">
+                            <p>Pricing</p>
+                            <p>Features</p>
+                            <p>Open Sources</p>
+                            <p>Contact</p>
+                        </div>
+                        <button
+                            className="bg-[#000] text-[#fff] text-sm px-8 py-2 rounded-full"
+                        >Sign In</button>
+                    </div>
+                    <div className="flex flex-col gap-7 mx-56">
+                        <h1 className="text-8xl w-1/3 font-bold">Make it awesome.</h1>
+                        <p className="text-2xl text-[#88888C]">AI trained to generate  realistic 3D objects.</p>
+                        <button
+                            className="flex justify-center gap-4 text-[#fff] bg-[#3D5CFF] w-40 p-4 rounded-full mt-6"
+                        > 
+                            <p>Get Started</p>
+                            <p>{'>'}</p>
+                        </button>
+                    </div>
+                    <div>
+                        <div>
+                            
+                        </div>
+                        <div>
+
+                        </div>
                     </div>
                 </div>
             </section>
