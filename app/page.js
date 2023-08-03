6ca56e844e3d8d24545568f9ed104ed8390c24ff
Dany <dany@gmail.com>
2023-08-02 20:09:30 -0600

    Terminando la prueba

--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
+'use client'
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/home')
+  }, [])
+
   return (
-    <div>
-      ok
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-8xl tracking-widest font-bold text-[#3D5CFF]">Byeeee!</h1>
     </div>
   )
 }
